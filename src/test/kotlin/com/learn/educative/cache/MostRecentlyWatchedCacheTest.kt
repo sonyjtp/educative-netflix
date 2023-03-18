b19084db85febae9c76d2919fa741e2a6b94a93e
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-03-17 23:47:49 -0500

    Updated MostRecentlyWatchedCacheTest

--- src/test/kotlin/com/learn/educative/cache/MostRecentlyWatchedCacheTest.kt
+++ src/test/kotlin/com/learn/educative/cache/MostRecentlyWatchedCacheTest.kt
@@ -7,35 +7,19 @@ import io.kotest.matchers.shouldBe
 class MostRecentlyWatchedCacheTest : FunSpec({
     test("addToCache") {
         val cache = MostRecentlyWatchedCache(2)
+        val expected1 = LinkedListNode(1, 10)
+        val expected2 = LinkedListNode(2, 20, prev = expected1)
+        val expected3 = LinkedListNode(3, 30, prev = expected2)
         cache.get(1) shouldBe null
         cache.set(1, 10)
-        cache.get(1) shouldBe LinkedListNode(1, 10, null, null)
+        cache.get(1) shouldBe LinkedListNode(1, 10, 0, null, null)
         cache.set(2, 20)
         cache.toString() shouldBe "MostRecentlyWatchedCache(" +
-                "cache:{" +
-                "1=LLNode(key:1, data:10, next:null, prev:null), " +
-                "2=LLNode(key:2, data:20, next:null, prev:1)}, " +
-                "cachedNodes:DLL(head:LLNode(key:1, data:10, next:2, prev:null), " +
-                "tail:LLNode(key:2, data:20, next:null, prev:1), " +
-                "size:2))"
+                "cache:{1=$expected1, 2=$expected2}, " +
+                "cachedNodes:DLL(head:${LinkedListNode(1, 10, next = expected2)}, tail:$expected2, size:2))"
         cache.set(3, 30)
         cache.toString() shouldBe "MostRecentlyWatchedCache(" +
-                "cache:{" +
-                "2=LLNode(key:2, data:20, next:3, prev:null), " +
-                "3=LLNode(key:3, data:30, next:null, prev:2)}, " +
-                "cachedNodes:DLL(head:LLNode(key:2, data:20, next:3, prev:null), " +
-                "tail:LLNode(key:3, data:30, next:null, prev:2), " +
-                "size:2))"
-        cache.set(2, 22)
-        cache.toString() shouldBe "MostRecentlyWatchedCache(" +
-                "cache:{" +
-                "2=LLNode(key:2, data:22, next:null, prev:3), " +
-                "3=LLNode(key:3, data:30, next:2, prev:null)}, " +
-                "cachedNodes:DLL(head:LLNode(key:3, data:30, next:2, prev:null), " +
-                "tail:LLNode(key:2, data:22, next:null, prev:3), " +
-                "size:2))"
+                "cache:{2=${LinkedListNode(2, 20, next = expected3)}, 3=$expected3}, " +
+                "cachedNodes:DLL(head:${LinkedListNode(2, 20, next = expected3)}, tail:$expected3, size:2))"
     }
-
-
-
 })
