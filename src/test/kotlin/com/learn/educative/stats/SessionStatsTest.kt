1cbdaca921dd19988be576d4cd49375940fc149f
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-04-07 20:03:00 -0500

    Fixed SessionStatsTest test cases

--- src/test/kotlin/com/learn/educative/stats/SessionStatsTest.kt
+++ src/test/kotlin/com/learn/educative/stats/SessionStatsTest.kt
@@ -18,10 +18,11 @@ internal class SessionStatsTest : FunSpec({
 
     test("findSessionEventsMedians") {
         forAll (
-            row(listOf(1, 2), 1, listOf(1.0, 2.0)),
+//            row(listOf(1, 2), 1, listOf(1.0, 2.0)),
+            row(listOf(1, 3, -1, -3, 5, 3, 6, 7), 3, listOf(1.0, -1.0, -1.0, 3.0, 5.0, 6.0)),
         ) { eventCounts, windowSize, result ->
             val sessionWindow = SessionWindow(
-                minQueue =  PriorityQueue(), maxQueue = PriorityQueue(), EventType.UNKNOWN,
+                minQueue =  PriorityQueue(Collections.reverseOrder()), maxQueue = PriorityQueue(), EventType.UNKNOWN,
                 windowSize = windowSize, eventCounts = eventCounts)
             sessionStats.findSessionEventsMedians(sessionWindow) shouldBe result
         }
