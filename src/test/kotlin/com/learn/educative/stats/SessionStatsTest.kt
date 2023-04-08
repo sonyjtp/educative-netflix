060ffcda8f353b87b13a69316398e872e476f3d5
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-04-07 19:26:49 -0500

    Updated testcases for SessionStats.kt

--- src/test/kotlin/com/learn/educative/stats/SessionStatsTest.kt
+++ src/test/kotlin/com/learn/educative/stats/SessionStatsTest.kt
@@ -3,6 +3,9 @@ package com.learn.educative.stats
 import com.learn.educative.dataclass.EventType
 import com.learn.educative.dataclass.SessionWindow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 import java.util.*
 
 internal class SessionStatsTest : FunSpec({
@@ -14,11 +17,13 @@ internal class SessionStatsTest : FunSpec({
     }
 
     test("findSessionEventsMedians") {
-        val sessionWindow = SessionWindow(
-            minQueue =  PriorityQueue(), maxQueue = PriorityQueue(),
-            EventType.UNKNOWN, windowSize = 3, eventCounts = listOf(1, 3, -1, -3, 5, 3, 6, 7)
-        )
-        print(sessionStats.findSessionEventsMedians(sessionWindow))
+        forAll (
+            row(listOf(1, 2), 1, listOf(1.0, 2.0)),
+        ) { eventCounts, windowSize, result ->
+            val sessionWindow = SessionWindow(
+                minQueue =  PriorityQueue(), maxQueue = PriorityQueue(), EventType.UNKNOWN,
+                windowSize = windowSize, eventCounts = eventCounts)
+            sessionStats.findSessionEventsMedians(sessionWindow) shouldBe result
+        }
     }
-
 })
